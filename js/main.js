8c7382166ca8e9399b4b781923f3a1fa6c0115aa
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2023-08-04 15:53:08 -0400

    set up margins, heights, widths, grouping

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
-/*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    Project 2 - Gapminder Clone
-*/
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 };
+const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM;
 
-d3.json("data/data.json").then(function(data){
-	console.log(data);
-})
+const svg = d3
+	.select('#chart-area')
+	.append('svg')
+	.attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+	.attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
+
+const g = svg
+	.append('g')
+	.attr('transform', `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
+
+d3.json('data/data.json').then((data) => {
+	const formattedData = data.map((data) =>
+		data['countries']
+			.filter((country) => {
+				const availableData = country.income && country.life_exp;
+				return availableData;
+			})
+			.map((data) => {
+				// console.log(data);
+				data.income = Number(data.income);
+				data.life_exp = Number(data.life_exp);
+				return data;
+			})
+	);
+
+	const circles = d3.selectAll('circle').data(formattedData);
+
+	circles
+		.enter()
+		.append('circle')
+		.attr('fill', 'grey')
+		.attr('cy', 0)
+		.attr('r', 5);
+
+	console.log(circles);
+});
